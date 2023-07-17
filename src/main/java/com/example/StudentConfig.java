201bbf815110c557ffc81c449ca38de349c8f980
umair.tariq <umair.tariq@techno-soft.com>
2023-07-17 18:59:21 +0500

    Comments added

--- src/main/java/com/example/StudentConfig.java
+++ src/main/java/com/example/StudentConfig.java
@@ -9,6 +9,14 @@ import java.time.LocalDate;
 import java.util.List;
 
 
+/*
+@Configuration Annotation:
+The @Configuration annotation marks the class as a Spring configuration class. It tells Spring that this class provides bean definitions and configuration settings.
+
+CommandLineRunner and @Bean:
+The CommandLineRunner interface in Spring Boot allows you to execute code when the application starts up. It provides a single method run() that is called with the command-line arguments when the application is started.
+* */
+
 @Configuration
 public class StudentConfig {
     @Bean
